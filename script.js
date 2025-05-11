dbe9218f4c5eefe21f5ac76397b69b2aba14f380
keshavnagar <keshavnagar887@gmail.com>
2025-05-11 23:47:30 +0530

    add preventdefault and declare some fields like const link , status and so on for target them for next process

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const addQuestionButton = document.getElementById("add-question-button");
 const addQuestionForm = document.getElementById("add-question-form");
+const questionName = document.getElementById("question-name")
+const topic = document.getElementById("topic")
+const difficulty = document.getElementById("difficulty")
+const platform = document.getElementById("platform")
+const link = document.getElementById("link")
+const status = document.getElementById("status")
+const date = document.getElementById("date")
+const notes = document.getElementById("notes")
+
 addQuestionButton.addEventListener("click", function () {
   addQuestionForm.style.visibility = "visible";
   addQuestionButton.style.visibility = "hidden";
 });
+
+
+addQuestionForm.addEventListener("submit",function(event){
+  event.preventDefault();
+})
+
