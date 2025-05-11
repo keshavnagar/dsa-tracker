6b52a0f82529e9ba7271de0b6baea1064928da6c
keshavnagar <keshavnagar887@gmail.com>
2025-05-11 22:09:03 +0530

    add , add question button and enter the functionality like when i click on button the form appear and this is done by css and java script

---
+++ script.js
@@ -0,0 +1,5 @@
+const addQuestionButton = document.getElementById("add-question-button");
+const addQuestionForm = document.getElementById("add-question-form");
+addQuestionButton.addEventListener("click", function () {
+  addQuestionForm.style.display = "block";
+});
